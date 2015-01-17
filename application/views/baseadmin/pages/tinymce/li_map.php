b8b9725816ac178dff4b0d083c6ab62d7381f45b
harryosmar <if07087@gmail.com>
2015-01-17 09:41:03 +0700

    Revert "first push simpled"
    This reverts commit cf8fda54d73983d01ebf6c651fd608a878513b77.

--- application/views/baseadmin/pages/tinymce/li_map.php
+++
@@ -1,6 +0,0 @@
-<li class="col-md-3 text-center <?php echo isset($new_upload_file) ? "new-upload-file" : ""; ?>" name="tinymce-media-img-li">
-    <img class="img-responsive" src="<?php echo "{$upload_url}{$file}"; ?>">
-    <label class="checkbox">
-        <input type="checkbox" name="tinymce-media-dialog-checkbox-img" img_url="<?php echo "{$upload_url}{$file}"; ?>" filename="<?php echo $file; ?>"><a class="filename-text prevent-default" href="#" title="<?php echo $file; ?>"><?php echo $file; ?></a>
-    </label>
-</li>
