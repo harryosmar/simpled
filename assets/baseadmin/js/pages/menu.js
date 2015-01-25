da7936d14fbd69b28e00e94d970f129bd06e83fd
Harry Sitohang <harry@olx.co.id>
2015-01-25 13:48:56 +0700

    fix icon-panel & breadcrumb

--- assets/baseadmin/js/pages/menu.js
+++ assets/baseadmin/js/pages/menu.js
@@ -1,16 +1,31 @@
 var menu = {
 	menu : function(){
-		$("#icon-panel-modal").modal('show');
+		var self = this;
 
 		$(document).on('click', '#icon-panel-modal li', function(){
-			var icon_class = $(this).find('i').attr('class');
+			var menu_icon = $(this).find('i').attr('class');
+			var menu_id = $("#icon-panel-modal [name=menu_id]").val();
+			self.ajax_request(class_url+"update_menu_icon", {
+				menu_id : menu_id,
+				menu_icon : menu_icon
+			});
+			$(this).addClass('active');
 		});
 
 		$(document).on('click', '[data-name=update-menu-icon]', function(){
-			
+			$("#icon-panel-modal [name=menu_id]").val($(this).attr('data-menu-id'));
+			var icon = $('#icon-panel-modal li i.'+$(this).attr('data-menu-icon'));
+			var li = icon.parent('li');
+			$('#icon-panel-modal li').removeClass('active');
+			li.addClass('active');
+			$("#icon-panel-modal").modal('show');
 		});
-
-		
+	},
+	onCompleteUpdateMenu : function(json){
+		var self = this;
+		$('ul.mainnav').html(json.subnavbar);
+		self.growl_msg(json.status, json.status, json.msg);
+		menu_datatable.refresh_datatable();
 	}
 };
 
